bcb3865c3fb545de6b395811d124507b143b1b9e
ZoitovNurillo <nurillozoitov97@gmail.com>
2024-06-17 21:39:24 +0500

    Update js code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 let cardLink = document.querySelectorAll(".card__link");
 let cardQuantity = document.getElementById("card__quantity");
-let cardMessage = document.getElementById("card__message")
+let cardMessage = document.getElementById("card__message");
+let cardLinkActive = document.querySelectorAll(".card__link--active");
+
+let a = 3;
+cardLink.forEach((i) => {
+  i.addEventListener("click", function() {
+    i.classList.remove("card__link--active");
+    cardQuantity.innerHTML = --a;
+    if(a === 0) {
+    cardQuantity.classList.add("card__quantity--d");
+    }
+  });
+})
+
+
 let cardClick = document.getElementById("card__link--js").addEventListener("click", function() {
   cardMessage.classList.toggle("card__message--active")
 });
